5a68c7100639cc4571a68d4850cb4ebb7482b45d
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-05-16 16:35:30 +0800

    jsdom to jsdom-core

--- sample.js
+++ sample.js
@@ -11,29 +11,6 @@
 
   const sis = new SIS(process.env.USERNAME, process.env.PASSWORD)
 
-  var res = await Promise.all([
-    sis.getUser(),
-    sis.getGrades(),
-    sis.getBalance(),
-    sis.getRegistration(),
-    sis.getCurriculum(),
-    sis.searchClassOfferings('4-%')
-  ])
-
-  var user = res[0]
-  var grades = res[1]
-  var balance = res[2]
-  var registration = res[3]
-  var curriculum = res[4]
-  var search = res[5]
-
-  console.log(user.all())
-  console.log(grades.all())
-  console.log(balance.all())
-  console.log(registration.all())
-  console.log(curriculum.all())
-  console.log(search.all()) 
-  
-  var prereg = await sis.getPrereg()
-  console.log(prereg.find({ name: 'CS 2136' }))
+  var prereg = await sis.searchPrereg('ASF')
+  console.log(prereg.response.all())
 })()
