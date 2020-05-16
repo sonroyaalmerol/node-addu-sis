dca8597ed52c6bbde4f127a2fb2e5530450c2612
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-05-16 23:39:08 +0800

    back

--- sample.js
+++ sample.js
@@ -14,27 +14,4 @@
   var isValid = await sis.checkAuth()
 
   console.log(isValid)
-
-  //var prereg = await sis.getUser()
-  //console.log(prereg.all())
-
-  /* const nodeFetch = require('node-fetch')
-  const fetch = require('fetch-cookie/node-fetch')(nodeFetch)
-
-  const qs = require('querystring')
-
-  const data = {
-    name: 'sonraalmerol',
-    pass: 'AdDU2201901285413',
-    form_id: 'user_login_block',
-    op: 'Log in'
-  }
-  var x = await fetch(`https://sis2.addu.edu.ph/node?destination=node`, { 
-    method: 'POST',
-    body: qs.stringify(data),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  })
-  console.log(await x.text()) */
 })()
