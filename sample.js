4c91c8cc262902e44b47b197e3ac20e1ec75b56b
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-05-16 22:47:41 +0800

    update dependencies

--- sample.js
+++ sample.js
@@ -11,28 +11,9 @@
 
   const sis = new SIS(process.env.USERNAME, process.env.PASSWORD)
 
-  var res = await Promise.all([
-    sis.getUser(),
-    sis.getGrades(),
-    sis.getBalance(),
-    sis.getRegistration(),
-    sis.getCurriculum(),
-    sis.searchClassOfferings('4-%')
-  ])
+  var isValid = await sis.checkAuth()
 
-  var user = res[0]
-  var grades = res[1]
-  var balance = res[2]
-  var registration = res[3]
-  var curriculum = res[4]
-  var search = res[5]
-
-  console.log(user.all())
-  console.log(grades.all())
-  console.log(balance.all())
-  console.log(registration.all())
-  console.log(curriculum.all())
-  console.log(search.all())
+  console.log(isValid)
 
   //var prereg = await sis.getUser()
   //console.log(prereg.all())
